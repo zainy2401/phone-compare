c98ec864c307950150775945b7cd14f66e4a885a
zainy2401 <zainuaziz2401@outlook.com>
2026-06-08 00:20:11 +0100

    Initial Push
    HTML skeleton, basic positioning in css and main js function created. For future, make responsive.

---
+++ www/script.js
@@ -0,0 +1,28 @@
+function Submit()
+{
+    let upFront = Number(document.getElementById("upfront").value);
+    let monthly = Number(document.getElementById("monthly").value);
+    let monthly1 = Number(document.getElementById("monthly1").value);
+    let monthly2 = Number(document.getElementById("monthly2").value);
+    let cash = Number(document.getElementById("cashPrice").value);
+    let sim = Number(document.getElementById("simOnly").value);
+    let result1 = document.getElementById("result1");
+    let result2 = document.getElementById("result2");
+
+    const d = new Date();
+    let setMonth = d.getMonth() + 1;
+    let contractLength = 24;
+    let set = true;
+    let counter = 0;
+    let firstSet = ((12-setMonth + 3)%12)+1;
+    let secondSet = 12;
+    let thirdSet = contractLength - firstSet - secondSet;
+
+    alert(upFront, monthly, monthly1, monthly2, cash, sim);
+
+    let calc1 = (firstSet*monthly) + (secondSet*monthly1) + (thirdSet*monthly2) + upFront;
+    result1.innerHTML = "£" + calc1.toFixed(2);
+
+    let calc2 = cash + (sim*24);
+    result2.innerHTML = "£" + calc2.toFixed(2);
+}
